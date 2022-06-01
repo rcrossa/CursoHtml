08953586c878d7a0386dbacc9f956c8aa6f54087
Roberto Rossa <rcrossa@hotmail.com>
2022-05-31 22:46:45 -0300

    Desafio Seo

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-const elementos = document.getElementsByClassName("selected");
-    for (let i = 0; elementos.length; i++) {
-    elementos[i].addEventListener("mouseover", selected);
-    
-    }
-function selected() {
-  this.style.color = "white";
-
-}
